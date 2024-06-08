4593fe7aaf5b92983a01d735ca8ce90f2e9ed9f8
Asad1400 <147860328+Asad1400@users.noreply.github.com>
2024-06-09 01:23:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+#! usr/bin/env node
+//shabang
+//npm is used to manage and install packages
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "Guess the number: ",
+        type: "number",
+        name: "num"
+    }
+]);
+const numberGuessingFunction = () => {
+    if (Math.random() * 10 === answer.num)
+        console.log("You guessed it right!");
+    else
+        console.log("You guessed it wrong!");
+};
+numberGuessingFunction();
